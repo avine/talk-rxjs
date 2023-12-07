e2091b662aa314c57d2b430c91228d7e641d6c53
Stéphane Francel <stephane.francel@gmail.com>
2023-12-07 15:23:52 +0100

    Improve CSS (test dark theme)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import "./node_modules/reveal.js/dist/reveal.css";
 import "./styles/theme.scss";
 
 // Highlight
-import "./node_modules/highlight.js/styles/atom-one-light.css";
+import "./node_modules/highlight.js/styles/atom-one-dark.css";
 
 // CopyCode
 import "./plugins/copy-code/copy-code.scss";
